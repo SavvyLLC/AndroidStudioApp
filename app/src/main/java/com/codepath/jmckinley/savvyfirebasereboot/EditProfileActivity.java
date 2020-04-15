65cfa234c68a210b84e422a76f98415c46aeac6e
pandarati <akajonathanmckinley@gmail.com>
2020-04-15 19:29:05 -0400

    Added capability of pulling profile details before editing profile & properly added functionality to update profile details

--- app/src/main/java/com/codepath/jmckinley/savvyfirebasereboot/EditProfileActivity.java
+++ app/src/main/java/com/codepath/jmckinley/savvyfirebasereboot/EditProfileActivity.java
@@ -7,11 +7,16 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -20,6 +25,7 @@ import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.squareup.picasso.Picasso;
 
 import javax.annotation.Nullable;
 
@@ -31,16 +37,22 @@ public class EditProfileActivity extends AppCompatActivity {
     private FirebaseAuth mAuth;
     private String userId;
 
-    private EditText etFullName;
+    private EditText etFirstName;
+    private EditText etLastName;
     private EditText etMajor;
     private EditText etUniversity;
-    private EditText etAboutMe;
-    private TextView tvEmail;
+    private EditText etBriefBio;
     private Button btnUploadResume;
     private Button btnCancel;
     private Button btnSubmit;
     private Button btnSignout;
     private ImageView ivProfileImage;
+
+    RadioGroup radioGroup;
+    RadioButton isCompanyRadioBtn;
+    RadioButton isStudentRadioBtn;
+
+    boolean isCompanyInitialValue = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -52,32 +64,31 @@ public class EditProfileActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();             // initialize auth
 
         // connect layout objects to java file
-        etFullName = findViewById(R.id.et_FullName);
-        etMajor = findViewById(R.id.editText_Major);
-        etUniversity = findViewById(R.id.editText_University);
-        etAboutMe = findViewById(R.id.editText_AboutMe);
+        etFirstName = findViewById(R.id.editProfileFirstName);
+        etLastName = findViewById(R.id.editProfileLastName);
+        etMajor = findViewById(R.id.editProfileMajor);
+        etUniversity = findViewById(R.id.editProfileUniversity);
+        etBriefBio = findViewById(R.id.editProfileBriefBio);
         btnUploadResume = findViewById(R.id.button_UploadResume);
-        btnCancel = findViewById(R.id.button_Cancel);
-        btnSubmit = findViewById(R.id.button_Submit);
-        btnSignout = findViewById(R.id.btnSignout);
-        ivProfileImage = findViewById(R.id.imageView_ProfileImage);
-        tvEmail = findViewById(R.id.tvEmail);
+        btnCancel = findViewById(R.id.editProfileCancel);
+        btnSubmit = findViewById(R.id.editProfileSubmitButton);
+        btnSignout = findViewById(R.id.editProfileSignOut);
+        ivProfileImage = findViewById(R.id.editProfileUserImage);
+        String imageUrl = "https://www.gannett-cdn.com/presto/2019/09/11/USAT/ab5c4363-b8ec-40b4-a617-4e0b08a3aa4b-AP_Kevin_Hart_Crash.JPG";
+        Picasso.get().load(imageUrl).into(ivProfileImage);
 
+        radioGroup = findViewById(R.id.editProfileAccountTypeRadioGroup);
+        isCompanyRadioBtn = findViewById(R.id.editProfileCompanyRadioButton);
+        isStudentRadioBtn = findViewById(R.id.editProfileStudentRadioButton);
+        
+        //TODO Remove after done with testing(Instant Login for Testing)
+        mAuth.signInWithEmailAndPassword("test@gmail.com", "123456");
         userId = mAuth.getCurrentUser().getUid();
 
         DocumentReference documentReference = fStore.collection("users").document(userId);
 
-        // read current user data from database
-        documentReference.addSnapshotListener(this, new EventListener<DocumentSnapshot>() {
-            @Override
-            public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
-                tvEmail.setText((documentSnapshot.getString("email")));
-                etFullName.setText((documentSnapshot.getString("name")));
-                etMajor.setText((documentSnapshot.getString("major")));
-                etAboutMe.setText((documentSnapshot.getString("about")));
-                etUniversity.setText((documentSnapshot.getString("university")));
-            }
-        });
+        loadUserDetailsIntoView();
+
 
         //Transitions to file upload screen
         btnUploadResume.setOnClickListener(new View.OnClickListener() {
@@ -93,10 +104,7 @@ public class EditProfileActivity extends AppCompatActivity {
         btnSubmit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String name = etFullName.getText().toString().trim();
-                String major = etMajor.getText().toString().trim();
-                String university = etUniversity.getText().toString().trim();
-                String about = etAboutMe.getText().toString().trim();
+                updateProfileChanges();
                 // TODO: set user profile image
 
                 // TODO: update information in database
@@ -126,6 +134,74 @@ public class EditProfileActivity extends AppCompatActivity {
             }
         });
 
+    }
+
+    //TODO Do an error check for invalid fields going to documentReference
+    public boolean updateProfileChanges(){
+
+        final DocumentReference docRef = fStore.collection("users").document(mAuth.getUid());
+
+        // Set the "isCapital" field of the city 'DC'
+        docRef.update("firstName", etFirstName.getText().toString().trim());
+        docRef.update("lastName", etLastName.getText().toString().trim());
+        docRef.update("major", etMajor.getText().toString().trim());
+        docRef.update("School", etUniversity.getText().toString().trim());
+        docRef.update("briefBio", etBriefBio.getText().toString().trim());
+
+        //If they don't equal, there was a change in account type
+        if(isCompanyInitialValue != isCompanyRadioBtn.isChecked()){
+            docRef.update("isCompany", !isCompanyInitialValue);
+        }
+
+
+        return true;
+    }
+
+    /*
+    Pulls from Firebase to load the users details into the view
+     */
+    public boolean loadUserDetailsIntoView(){
+
+        final DocumentReference docRef = fStore.collection("users").document(mAuth.getUid());
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+
+
+                        //Loads information into layout
+                        etFirstName.setText(document.get("firstName").toString());
+                        etLastName.setText(document.get("lastName").toString());
+                        etMajor.setText(document.get("major").toString());
+                        etUniversity.setText(document.get("School").toString());
+                        etBriefBio.setText(document.get("briefBio").toString());
+
+                        boolean isCompany = (boolean)document.get("isCompany");
+                        if(isCompany) {
+                            isStudentRadioBtn.setChecked(false);
+                            isCompanyRadioBtn.setChecked(true);
+                            isCompanyInitialValue = true;
+                        }
+                        else {
+                            isCompanyRadioBtn.setChecked(false);
+                            isStudentRadioBtn.setChecked(true);
+                            isCompanyInitialValue= false;
+                        }
+
+
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+
+        return true;
     }
 
     public void updateProfile(){
