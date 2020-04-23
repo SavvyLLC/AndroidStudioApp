11437f9a1a5caabbe459dcd76babc433d3ea8163
pandarati <akajonathanmckinley@gmail.com>
2020-04-22 20:18:58 -0400

    VideoCalling feature basis code

---
+++ app/src/main/java/com/codepath/jmckinley/savvyfirebasereboot/activities/VideoCall_Content_Main.java
@@ -0,0 +1,31 @@
+package com.codepath.jmckinley.savvyfirebasereboot.activities;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+//import com.vidyo.VidyoClient.Connector.VidyoConnector;
+//import com.vidyo.VidyoClient.Connector.Connector;
+
+
+
+
+
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class VideoCall_Content_Main extends AppCompatActivity {
+
+//    private VideoConnector vc;
+
+    Button connectBtn;
+    Button startBtn;
+    Button disconnectBtn;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+//        ConnectorPkg.setApplicationUIContext(this);
+//        ConnectorPkg.initialize();
+    }
+
+}
